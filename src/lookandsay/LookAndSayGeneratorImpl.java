51deadae6d110a47464972ae6fcb02f4d97fe17b
kjaron83 <kjaron83@gmail.com>
2021-01-19 20:49:03 +0100

    Implementing the lookAndSay() method in the LookAndSayGeneratorImpl class.

--- src/lookandsay/LookAndSayGeneratorImpl.java
+++ src/lookandsay/LookAndSayGeneratorImpl.java
@@ -15,7 +15,38 @@ public class LookAndSayGeneratorImpl implements LookAndSayGenerator {
 
     @Override
     public String lookAndSay(CharSequence member) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if ( member == null )
+            return null;
+
+        if ( member.length() == 0 )
+            return "";
+        
+        if ( member.length() == 1 )
+            return "1" + member;
+        
+        return generateLookAndSay(member);
+    }
+
+    private String generateLookAndSay(CharSequence member) {
+        int length = member.length();
+
+        StringBuilder sb = new StringBuilder(length * 2);
+
+        int counter = 1;
+        char lastChar = member.charAt(0);
+        char currentChar;
+        for ( int i = 1; i < member.length(); i++ ) {
+            currentChar = member.charAt(i);
+            if ( currentChar != lastChar ) {
+                sb.append(counter).append(lastChar);
+                lastChar = currentChar;
+                counter = 1;
+            }
+            else
+                counter++;
+        }
+        
+        return sb.append(counter).append(lastChar).toString();
     }
 
 }
